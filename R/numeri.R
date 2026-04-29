885eef4cedee3bce8af19c904d2291387a6b4762
mar-tor-e <marta.marrone2@studio.unibo.it>
2026-04-29 17:39:38 +0200

    Create numeri.R

---
+++ R/numeri.R
@@ -0,0 +1,13 @@
+#funzione numeri
+
+numeri = function(x) {
+  if(x>0) {
+  print("Questo numero è positivo")
+    }
+  else if(x<0) {
+  print("Questo numero è negativo")
+    }
+  else {
+  print("È zero")
+    }
+  }
